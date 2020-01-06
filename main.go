e52de593cd5dbddc605e3d39b1d85cb8682a7821
gwind <gwindfree@gmail.com>
2020-01-06 23:33:40 +0800

    use cobra ; add dep

--- main.go
+++ main.go
@@ -1,57 +1,76 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/codegangsta/cli"
+	"github.com/Sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
+// rootCmd 是主命令对象
+var rootCmd = &cobra.Command{
+	Use:   "goproxy SUBCOMMAND ARGS",
+	Short: "非常容易使用的代理工具",
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "verbose output")
+	cobra.OnInitialize(initRootConfig)
+}
+
+func initRootConfig() {
+	viper.SetEnvPrefix("SUPERVISOR") // will be uppercased automatically
+	viper.BindEnv("DEBUG")
+	viper.BindPFlag("debug", rootCmd.Flags().Lookup("verbose"))
+
+	verbose := viper.GetBool("debug")
+	if verbose {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+}
+
 func main() {
-	app := cli.NewApp()
-	app.Name = "goproxy"
-	app.Usage = "非常容易使用的代理工具"
 
-	listenFlag := cli.StringFlag{
-		Name:  "listen, l",
-		Value: "127.0.0.1:9000",
-		Usage: "代理监听地址",
-	}
-
-	app.Commands = []cli.Command{
-		{
-			Name:    "http",
-			Aliases: []string{"hp"},
-			Usage:   "启动 HTTP(s) 代理",
-			Action: func(c *cli.Context) {
-				listen := c.String("listen")
-				backend := c.String("backend")
-				if backend == "" {
-					startHttpProxy(listen)
-				} else {
-					startHttpProxyByBackend(listen, backend)
-				}
-			},
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:   "backend, b",
-					Value:  "",
-					Usage:  "匹配后端 socks v5 代理",
-					EnvVar: "SOCKS_PROXY",
-				},
-				listenFlag,
-			},
-		},
-		{
-			Name:    "socks",
-			Aliases: []string{"s"},
-			Usage:   "启动 socks(v5) 代理",
-			Action: func(c *cli.Context) {
-				listen := c.String("listen")
-				startSocksV5(listen)
-			},
-			Flags: []cli.Flag{listenFlag},
+	// For http
+	var httpCmd = &cobra.Command{
+		Use:   "http ARGS",
+		Short: "启动 HTTP(s) 代理",
+		Run: func(cmd *cobra.Command, args []string) {
+			listen := viper.GetString("http_listen")
+			backend := viper.GetString("backend")
+			if backend == "" {
+				startHttpProxy(listen)
+			} else {
+				startHttpProxyByBackend(listen, backend)
+			}
 		},
 	}
 
-	app.Run(os.Args)
+	httpCmd.Flags().String("backend", "", "匹配后端 socks v5 代理")
+	viper.BindPFlag("SOCKS_PROXY", httpCmd.Flags().Lookup("backend"))
+
+	httpCmd.Flags().String("listen", "127.0.0.1:9000", "代理监听地址")
+	viper.BindPFlag("HTTP_LISTEN", httpCmd.Flags().Lookup("listen"))
+
+	// For socks
+	var socksCmd = &cobra.Command{
+		Use:   "socks ARGS",
+		Short: "启动 socks(v5) 代理",
+		Run: func(cmd *cobra.Command, args []string) {
+			listen := viper.GetString("socks_listen")
+			startSocksV5(listen)
+		},
+	}
+	socksCmd.Flags().String("listen", "127.0.0.1:8000", "代理监听地址")
+	viper.BindPFlag("SOCKS_LISTEN", socksCmd.Flags().Lookup("listen"))
+
+	rootCmd.AddCommand(httpCmd)
+	rootCmd.AddCommand(socksCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
---
+++ vendor/h12.io/socks/README.md
@@ -0,0 +1,58 @@
+SOCKS
+=====
+
+[![GoDoc](https://godoc.org/h12.io/socks?status.svg)](https://godoc.org/h12.io/socks)
+
+SOCKS is a SOCKS4, SOCKS4A and SOCKS5 proxy package for Go.
+
+## Quick Start
+### Get the package
+
+    go get -u "h12.io/socks"
+
+### Import the package
+
+    import "h12.io/socks"
+
+### Create a SOCKS proxy dialing function
+
+    dialSocksProxy := socks.DialSocksProxy(socks.SOCKS5, "127.0.0.1:1080")
+    tr := &http.Transport{Dial: dialSocksProxy}
+    httpClient := &http.Client{Transport: tr}
+
+## Example
+
+```go
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"h12.io/socks"
+)
+
+func main() {
+	dialSocksProxy := socks.DialSocksProxy(socks.SOCKS5, "127.0.0.1:1080")
+	tr := &http.Transport{Dial: dialSocksProxy}
+	httpClient := &http.Client{Transport: tr}
+	resp, err := httpClient.Get("http://www.google.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal(resp.StatusCode)
+	}
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(buf))
+}
+```
+
+## Alternatives
+http://godoc.org/golang.org/x/net/proxy
